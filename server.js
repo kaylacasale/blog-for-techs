c83f1ba090b761af13ae1f1be07cf504955792b4
Kayla Casale <kayla.casale@gmail.com>
2023-01-20 04:27:22 -0800

    after running through node server.js in terminal several times, I realized I needed to download several packages I such as express-sessions and handlebars, but now seems to be running sequelize

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+
+//* require express-sessions
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
