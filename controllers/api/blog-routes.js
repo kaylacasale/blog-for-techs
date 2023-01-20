9fc58380a7c6380f26e45a85c96e93a5ed9f2c3a
Kayla Casale <kayla.casale@gmail.com>
2023-01-20 00:49:43 -0800

    added route to update existing blog post in blog-routes and reference in dashboard.js

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -16,6 +16,28 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(err);
     }
 });
+//* update blog data
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.update({
+            title: req.body.title,
+            content: req.body.content,
+
+
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog was found with id!' })
+            return;
+        }
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //* DELETE request - route to delete an existing blog post (by id)
 router.delete('/:id', withAuth, async (req, res) => {
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const blogRoutes = require('./blog-routes');
 const commentRoutes = require('./comment-routes')
 
 router.use('/users', userRoutes);
-router.use('/blog', blogRoutes)
+router.use('/blogs', blogRoutes)
 router.use('/comments', commentRoutes);
 
 module.exports = router;
