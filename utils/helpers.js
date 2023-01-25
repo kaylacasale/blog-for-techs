29c1d38c1a473845a31c221facd4f624982234fb
Kayla Casale <kayla.casale@gmail.com>
2023-01-24 20:01:30 -0800

    added styling to blog and comment form

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,18 @@
 //* to format date for user info (username, date create) of blog post
+//* require dayjs after installing in Node.js 
+//* added dependency with NPM: 'npm install dayjs'
+//* https://day.js.org/docs/en/installation/node-js
+
+const dayjs = require('dayjs');
+
 module.exports = {
     format_date: (date) => {
         // Format date as MM/DD/YYYY
         return date.toLocaleDateString();
+    },
+    //* to format time, will mostly format 'date_created' property of objects that contains time
+    format_time: (time) => {
+        return dayjs(time).format('h:mm A')
     },
     format_amount: (amount) => {
         // format large numbers with commas
