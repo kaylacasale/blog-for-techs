f9ad50af5ae51cbcad620af70c932a98f84a2791
Kayla Casale <kayla.casale@gmail.com>
2023-01-24 20:25:46 -0800

    added functionality to add comments to blog, but not displaying back on page

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,6 +1,7 @@
 //* to add a new comment (grabbing input from blog.handlebars, specifically new-comment-form) for POST request to add new comment
 //* to delete existing comment in separate "cooment.handlebars or deleteComment.handlebars"
 // const { DELETE, UPDATE, POST } = require("sequelize/types/query-types");
+//* this is more or less 'comment.js' b/c handles comment data, from blog.handlebars page
 
 const newFormHandler = async (event) => {
     event.preventDefault();
@@ -17,6 +18,7 @@ const newFormHandler = async (event) => {
                 'Content-Type': 'application/json',
             },
         });
+        console.log(comment)
 
         if (response.ok) {
             document.location.replace('/'); // was '/dashboard'
